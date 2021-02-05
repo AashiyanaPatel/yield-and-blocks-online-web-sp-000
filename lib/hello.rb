c7a769ac0699b58eaee141d263ea3072f319e81d
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 06:29:04 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,1 @@
-  
-def hello_t(array)
-    i = 0
-    while i < array.length
-      i = i + 1
-    end
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-# call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+
